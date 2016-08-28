a88e74634026deba1f3f0418ad72126bdc4a6b7d
ksmahesh <mahesh.kothagere@gmail.com>
2016-08-28 14:21:54 -0500

    Update CombinationLock.java

--- CombinationLock.java
+++ CombinationLock.java
@@ -1,12 +1,17 @@
 /*Combination Lock
-Consider a -slot combination lock where each slot contains a dial numbered with the ten sequential decimal integers in the 
-inclusive range from  to . In one operation, you can choose a slot and rotate the dial by one click, either in the positive 
-direction (to increase the displayed number by ) or the negative direction (to decrease the displayed number by ). 
-Note that, due to the cyclical nature of the dial, the next number after  is  and the number before  is ). 
-For example, if the number  is currently displayed on the dial, you can rotate the dial to either  (positive direction) or 
-(negative direction) in a single operation.
-Given the initial configuration of numbers in each slot and some desired configuration of numbers, determine the minimum number
-of operations you must perform to change the lock's slots to the desired configuration.
+
+Consider a -slot combination lock where each slot contains a dial numbered with the ten sequential decimal integers in the inclusive range from 0 to 9. 
+In one operation, you can choose a slot and rotate the dial by one click, either in the positive direction (to increase the displayed number by 1) 
+or the negative direction (to decrease the displayed number by 1). Note that, due to the cyclical nature of the dial, the next number after 9 is 0 and the number before 0 is 9). 
+For example, if the number 0 is currently displayed on the dial, you can rotate the dial to either 1 (positive direction) or 9 (negative direction) in a single operation.
+Given the initial configuration of numbers in each slot and some desired configuration of numbers, determine the minimum number of operations you must perform to 
+change the lock's slots to the desired configuration.
+Input Format:
+The first line contains  space-separated integers denoting the current slot configuration. 
+The second line contains  space-separated integers denoting the desired slot configuration.
+Output Format:
+Print a single integer denoting the minimum number of moves to change this configuration to the correct one.
+
 */
 
 import java.io.*;
